94b5d5e4035e6f50ec8078bc7bafe5682774c281
Matti Lankinen <m.lankinen@iki.fi>
2014-03-31 15:13:26 +0300

    load docs

--- src/ir_course/Program.java
+++ src/ir_course/Program.java
@@ -3,7 +3,19 @@ package ir_course;
 public class Program {
 
   public static void main(String[] args) {
-    System.out.println("Hello IR course!");
+    new Program().run();
+  }
+
+
+  public final DocumentCollectionParser parser = new DocumentCollectionParser();
+
+  public Program() {
+    System.out.println("Loading documents...");
+    parser.parse("corpus_part2.xml");
+  }
+
+  public void run() {
+    System.out.println("Documents: " + parser.getDocuments().size());
   }
 
 }
